524b2196071220dcfeb28ecb4c917ebba7f5f5ad
Seu Nome <seuemail@gmail.com>
2026-01-30 17:56:36 -0300

    fix> migrations

--- src/server/Server.ts
+++ src/server/Server.ts
@@ -3,12 +3,21 @@ import "dotenv/config";
 
 import './shared/services/TranslationsYup';
 import { router } from "./routes/";
+import { Knex } from './database/knex';
 
 
 const server = express();
 
+// Executa as migrations automaticamente ao iniciar
+Knex.migrate.latest()
+	.then(() => {
+		console.log('Migrations executadas com sucesso!');
+	})
+	.catch((err) => {
+		console.error('Erro ao executar migrations:', err);
+	});
+
 server.use(express.json());
 server.use(router);
-
 
 export { server };
