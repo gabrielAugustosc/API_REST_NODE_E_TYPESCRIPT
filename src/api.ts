47fd021293a71ad1a5c70fde456e416e56a523ce
Seu Nome <seuemail@gmail.com>
2026-01-30 17:28:23 -0300

    #

--- src/api.ts
+++ src/api.ts
@@ -3,5 +3,5 @@ import type { VercelRequest, VercelResponse } from '@vercel/node';
 
 // Adapt Express to Vercel serverless
 export default function handler(req: VercelRequest, res: VercelResponse) {
-  server(req, res);
+    server(req, res);
 }
