5de58bdc59c650d849ffa05184618ec4b592f3c1
Seu Nome <seuemail@gmail.com>
2026-01-30 17:47:52 -0300

    fix: resolve production problem

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,21 @@
+import { Knex } from "./server/database/knex";
 import { server } from "./server/Server";  
+
+
+const startServer = () => {
+
 
 server.listen(process.env.PORT || 3333, () => {
     console.log(`Server rodando na porta ${process.env.PORT || 3333}`);
 });
+};
+
+
+if (process.env.IS_LOCALHOST !== 'true') {
+Knex.migrate.latest().then(() => {
+    startServer();
+})
+.catch(console.log);
+}else {
+    startServer();
+}
