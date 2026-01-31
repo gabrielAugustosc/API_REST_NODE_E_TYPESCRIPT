17aad4409f730f9b5fdcaad816f24bb0015c924e
Seu Nome <seuemail@gmail.com>
2026-01-31 15:05:40 -0300

    fix

--- tests/jest.setup.ts
+++ tests/jest.setup.ts
@@ -5,11 +5,10 @@ import { Knex } from '../src/server/database/knex';
 beforeAll(async () => {
    await Knex.migrate.latest();
    await Knex.seed.run();
-})
+});
 
 afterAll(async () => {
    await Knex.destroy();
 });
 
 export const testServer = supertest(server);
-//
